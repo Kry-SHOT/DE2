bdc3f14c423a6c95ec7d97a13b43534f6895fb79
Kry-SHOT <krystof.michal@gmail.com>
2020-09-29 14:45:12 +0200

    morse code created

--- Labs/01-tools/blink/blink/main.c
+++ Labs/01-tools/blink/blink/main.c
@@ -1,12 +1,12 @@
 /***********************************************************************
- * 
+ *
  * Blink a LED and use the function from the delay library.
  * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
  * Copyright (c) 2018-2020 Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
- * 
+ *
  **********************************************************************/
 
 /* Defines -----------------------------------------------------------*/
@@ -38,15 +38,19 @@ int main(void)
     // PORTB = PORTB and 1101 1111
     PORTB = PORTB & ~(1<<LED_GREEN);
 
+    /* Morse code translated into delays; dots and pauses between dots and dashes are 250ms, dashes and pauses between
+    letters are 500ms, final pause (between words) is 1000ms*/
+    int code[]={500, 250, 250, 250, 250, 500, 250, 500, 250, 250, 250, 250, 500, 250, 500, 250, 500, 1000};
+
     // Infinite loop
     while (1)
     {
-        // Pause several miliseconds
-        _delay_ms(SHORT_DELAY);
-
         // Invert LED in Data Register
         // PORTB = PORTB xor 0010 0000
         PORTB = PORTB ^ (1<<LED_GREEN);
+
+        for(int i=0;i<sizeof(code);i++)
+            _delay_ms(code[i]);
     }
 
     // Will never reach this
